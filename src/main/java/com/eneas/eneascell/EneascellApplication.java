9f8f3154bb036b3565c32902ee4cf3a7dc032f7a
Mateus Enéas <127209274+MateusEneas@users.noreply.github.com>
2025-11-14 14:32:20 -0300

    Iniciando projeto

---
+++ src/main/java/com/eneas/eneascell/EneascellApplication.java
@@ -0,0 +1,13 @@
+package com.eneas.eneascell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EneascellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EneascellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eneas/eneascell/EneascellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eneas.eneascell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EneascellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
