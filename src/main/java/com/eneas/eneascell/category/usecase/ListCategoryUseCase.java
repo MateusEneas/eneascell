d006d15415c6c9d41773ff0a206201d09360c638
Mateus Enéas <127209274+MateusEneas@users.noreply.github.com>
2026-02-25 15:25:03 -0300

    Adicionando @Transactional e readOnly = true em listar as categorias

--- src/main/java/com/eneas/eneascell/category/usecase/ListCategoryUseCase.java
+++ src/main/java/com/eneas/eneascell/category/usecase/ListCategoryUseCase.java
@@ -5,6 +5,7 @@ import com.eneas.eneascell.category.mapper.CategoryMapper;
 import com.eneas.eneascell.category.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,6 +18,7 @@ public class ListCategoryUseCase {
     @Autowired
     private CategoryMapper mapper;
 
+    @Transactional(readOnly = true)
     public List<CategoryDTO> execute() {
         return categoryRepository.findAll()
                 .stream()
