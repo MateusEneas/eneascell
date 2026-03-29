532d8b2ae40b44f3fa83ded29f49ea2386328742
Mateus Enéas <127209274+MateusEneas@users.noreply.github.com>
2026-03-29 13:53:34 -0300

    Criando testes de deletar categoria por ID - shouldThrowExceptionWhenIdDoesNotExist

---
+++ src/test/java/com/eneas/eneascell/category/usecase/DeleteCategoryByIdUseCaseTests.java
@@ -0,0 +1,40 @@
+package com.eneas.eneascell.category.usecase;
+
+import com.eneas.eneascell.category.repositories.CategoryRepository;
+import com.eneas.eneascell.exceptions.NotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+public class DeleteCategoryByIdUseCaseTests {
+
+    @InjectMocks
+    private DeleteCategoryByIdUseCase useCase;
+
+    @Mock
+    private CategoryRepository repository;
+
+    private UUID existingId;
+    private UUID nonExistingId;
+
+    @BeforeEach
+    void setUp() {
+        existingId = UUID.randomUUID();
+        nonExistingId = UUID.randomUUID();
+    }
+
+    @Test
+    void shouldThrowExceptionWhenIdDoesNotExist() {
+        when(repository.existsById(nonExistingId)).thenReturn(false);
+        assertThrows(NotFoundException.class, () -> useCase.execute(nonExistingId));
+    }
+}
