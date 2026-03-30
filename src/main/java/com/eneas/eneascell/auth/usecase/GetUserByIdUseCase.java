fc88671ef1a4b047c246f30b641f51e80aa23bbf
Mateus Enéas <127209274+MateusEneas@users.noreply.github.com>
2026-03-30 17:24:33 -0300

    Criando use case de User - GetUserByIdUseCase

---
+++ src/main/java/com/eneas/eneascell/auth/usecase/GetUserByIdUseCase.java
@@ -0,0 +1,22 @@
+package com.eneas.eneascell.auth.usecase;
+
+import com.eneas.eneascell.auth.domain.User;
+import com.eneas.eneascell.auth.repository.UserRepository;
+import com.eneas.eneascell.exceptions.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class GetUserByIdUseCase {
+
+    private final UserRepository userRepository;
+
+    public User execute(UUID id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Usuário não encontrado"));
+    }
+
+}
