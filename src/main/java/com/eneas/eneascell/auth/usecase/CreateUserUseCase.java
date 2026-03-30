7dabfd65fdadc0369fa5e0a1b35fed1a26a75c08
Mateus Enéas <127209274+MateusEneas@users.noreply.github.com>
2026-03-30 17:09:02 -0300

    Criando os usecase de usuario - CreateUserUseCase

---
+++ src/main/java/com/eneas/eneascell/auth/usecase/CreateUserUseCase.java
@@ -0,0 +1,34 @@
+package com.eneas.eneascell.auth.usecase;
+
+import com.eneas.eneascell.auth.domain.User;
+import com.eneas.eneascell.auth.dto.CreateUserDTO;
+import com.eneas.eneascell.auth.repository.UserRepository;
+import com.eneas.eneascell.exceptions.BusinessException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CreateUserUseCase {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User execute(CreateUserDTO dto) {
+
+        if (userRepository.findByEmail(dto.email()).isPresent()) {
+            throw new BusinessException("Email já cadastrado");
+        }
+
+        User user = new User();
+        user.setNome(dto.nome());
+        user.setEmail(dto.email());
+        user.setSenha(passwordEncoder.encode(dto.senha()));
+        user.setRole(dto.role());
+
+        return userRepository.save(user);
+    }
+
+
+}
