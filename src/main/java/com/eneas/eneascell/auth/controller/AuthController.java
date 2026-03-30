00ba777f5ad0db0e71db51917607ddd801b58c29
Mateus Enéas <127209274+MateusEneas@users.noreply.github.com>
2026-03-30 16:51:30 -0300

    Criando a classe AuthController

---
+++ src/main/java/com/eneas/eneascell/auth/controller/AuthController.java
@@ -0,0 +1,36 @@
+package com.eneas.eneascell.auth.controller;
+
+import com.eneas.eneascell.auth.JwtService;
+import com.eneas.eneascell.auth.dto.LoginDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginDTO dto) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(dto.email(), dto.senha())
+            );
+
+            String token = jwtService.generateToken(dto.email());
+            return ResponseEntity.ok(token);
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.status(401).body("Email ou senha inválidos");
+        }
+    }
+}
