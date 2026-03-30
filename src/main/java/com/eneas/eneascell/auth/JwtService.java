ad539d778e6d8a32c6c9e45d77ff312cdc1122cd
Mateus Enéas <127209274+MateusEneas@users.noreply.github.com>
2026-03-30 17:45:59 -0300

    Refatorando JwtService

--- src/main/java/com/eneas/eneascell/auth/JwtService.java
+++ src/main/java/com/eneas/eneascell/auth/JwtService.java
@@ -3,10 +3,12 @@ package com.eneas.eneascell.auth;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 
+@Service
 public class JwtService {
 
     @Value("${jwt.secret}")
