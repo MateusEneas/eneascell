117c562842755279ecf2c09bad4f37d8328845de
Mateus Enéas <127209274+MateusEneas@users.noreply.github.com>
2026-04-11 13:13:52 -0300

    fix UpdateCategoryUseCase

--- src/main/java/com/eneas/eneascell/category/usecase/UpdateCategoryUseCase.java
+++ src/main/java/com/eneas/eneascell/category/usecase/UpdateCategoryUseCase.java
@@ -4,6 +4,7 @@ import com.eneas.eneascell.category.domain.Category;
 import com.eneas.eneascell.category.dto.CategoryDTO;
 import com.eneas.eneascell.category.mapper.CategoryMapper;
 import com.eneas.eneascell.category.repositories.CategoryRepository;
+import com.eneas.eneascell.exceptions.BusinessException;
 import com.eneas.eneascell.exceptions.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,6 +28,12 @@ public class UpdateCategoryUseCase {
                 .orElseThrow(() -> new NotFoundException("Categoria não encontrado!"));
 
         if (dto.getNome() != null && !dto.getNome().trim().isEmpty()) {
+            if (!dto.getNome().equals(category.getNome())) {
+                var categoriaExistente = categoryRepository.findByNome(dto.getNome());
+                if (categoriaExistente != null) {
+                    throw new BusinessException("Já existe uma categoria com esse nome!");
+                }
+            }
             category.setNome(dto.getNome());
         }
 
